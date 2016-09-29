5dc7931b39747384a52ea5f4a6f7c838a5c059f8
Janis <janis@jaggedverge.com>
2016-09-29 20:59:30 +1000

    Update old code to use newer enumerations

--- illustrations/web_app_too_broad.py
+++ illustrations/web_app_too_broad.py
@@ -1,18 +1,23 @@
-from knowledge_hierachy.knowledge_hierachy  import KnowledgeHierachy, Goal, Skill
-
+from knowledge_hierachy.knowledge_hierachy import (
+    KnowledgeHierachy,
+    Goal,
+    Skill,
+    KnowledgeState,
+    KnowledgeRelation,
+)
 example_goal = Goal("Create a complex single page web app")
 
-webdev_everything_skill = Skill("web development \n skills", "known")
+webdev_everything_skill = Skill("web development\nskills", KnowledgeState.UNKNOWN_AWARE)
 
 web_dev_too_broad1 = KnowledgeHierachy("Web development too broad", nodes=[webdev_everything_skill, example_goal])
-web_dev_too_broad1.add_edge(webdev_everything_skill, example_goal, "known")
+web_dev_too_broad1.add_edge(webdev_everything_skill, example_goal, KnowledgeRelation.KNOWN_CONNECTION)
 web_dev_too_broad1.create_image()
 
 
-backend_everything_skill = Skill("Backend \n skills", "unknown")
-frontend_everything_skill = Skill("Frontend \n skills", "unknown")
+backend_everything_skill = Skill("Backend \n skills", KnowledgeState.UNKNOWN_AWARE)
+frontend_everything_skill = Skill("Frontend \n skills", KnowledgeState.UNKNOWN_AWARE)
 web_dev_2_nodes = [backend_everything_skill, frontend_everything_skill, example_goal]
 web_dev_too_broad2 = KnowledgeHierachy("Web development still too broad", nodes=web_dev_2_nodes)
-web_dev_too_broad2.add_edge(frontend_everything_skill, example_goal, "known")
-web_dev_too_broad2.add_edge(backend_everything_skill, example_goal, "known")
+web_dev_too_broad2.add_edge(frontend_everything_skill, example_goal, KnowledgeRelation.KNOWN_CONNECTION)
+web_dev_too_broad2.add_edge(backend_everything_skill, example_goal, KnowledgeRelation.KNOWN_CONNECTION)
 web_dev_too_broad2.create_image()
