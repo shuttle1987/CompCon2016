aba3ec630604527bcb993bda05453378cc14db07
Janis <janis@jaggedverge.com>
2016-09-29 23:35:03 +1000

    Added python dependencies file

--- illustrations/photocopy.py
+++ illustrations/photocopy.py
@@ -5,7 +5,7 @@ example_goal = Goal("Photocopy documents")
 
 photocopy_skill = Skill("use \n photocopier", "known")
 
-example = KnowledgeHierachy("example", nodes=[photocopy_skill, example_goal])
+example = KnowledgeHierachy("Photocopying example", nodes=[photocopy_skill, example_goal])
 example.add_edge(photocopy_skill, example_goal, "known")
 example.create_image()
 
