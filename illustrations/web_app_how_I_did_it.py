fd4f8b3c0bbefab6c8082af820b03995bc18e317
Janis <janis@jaggedverge.com>
2016-09-29 16:30:42 +1000

    More accurate picture of how I actually did it

--- illustrations/web_app_how_I_did_it.py
+++ illustrations/web_app_how_I_did_it.py
@@ -1,30 +1,40 @@
 from knowledge_hierachy.knowledge_hierachy  import KnowledgeHierachy, Goal, Skill
 
-js_fundamentals = Skill("JS fundamentals")
-python_fundamentals = Skill("python fundamentals")
+web_app_goal = Goal("Make a web app that can display tree based information")
+
+js_fundamentals = Skill("Javascript \n  fundamentals", "known")
+python_fundamentals = Skill("Python \n fundamentals", "known")
 django_skill = Skill("Django \n backend", "known")
 frontend_skill = Skill("JS Frontend", "known")
-mptt_skill = Skill("MPTT", "known")
-d3_skill = Skill("D3", "known")
+mptt_skill = Skill("MPTT", "unknown")
+d3_skill = Skill("D3 library", "known")
+REST_api_skill = Skill("REST\nAPI", "known")
 web_dev_2_nodes = [
     js_fundamentals,
     python_fundamentals,
     d3_skill,
     mptt_skill,
-    backend_everything_skill,
-    frontend_everything_skill,
-    example_goal
+    django_skill,
+    frontend_skill,
+    REST_api_skill,
+    web_app_goal
 ]
 
-web_project_actual = KnowledgeHierachy("Web development still too broad", nodes=web_dev_2_nodes)
+web_project_actual = KnowledgeHierachy("Single page web app", nodes=web_dev_2_nodes)
 #backend
 web_project_actual.add_edge(python_fundamentals, django_skill, "known")
 web_project_actual.add_edge(mptt_skill, django_skill, "known")
-web_project_actual.add_edge(django_skill, example_goal, "known")
+web_project_actual.add_edge(django_skill, web_app_goal, "known")
 
 #frontend
 web_project_actual.add_edge(js_fundamentals, frontend_skill, "known")
 web_project_actual.add_edge(js_fundamentals, d3_skill, "known")
 web_project_actual.add_edge(d3_skill, frontend_skill, "known")
-web_project_actual.add_edge(frontend_skill, example_goal, "known")
+web_project_actual.add_edge(frontend_skill, web_app_goal, "known")
+
+#both frontend and backend
+web_project_actual.add_edge(REST_api_skill, frontend_skill, "known")
+web_project_actual.add_edge(REST_api_skill, django_skill, "known")
+
+
 web_project_actual.create_image()
